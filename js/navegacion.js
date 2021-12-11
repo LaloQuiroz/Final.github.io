61e01eca7f7e524d6bc778168df40aaea0d93d50
LaloQuiroz <94099093+LaloQuiroz@users.noreply.github.com>
2021-12-10 21:24:48 -0600

    Add files via upload

---
+++ js/navegacion.js
@@ -0,0 +1,14 @@
+export function
+  muestraAlumnos() {
+  location.href =
+    "alumnos.html";
+}
+
+export function
+  muestraUsuarios() {
+  location.href = "usuarios.html";
+}
+
+export function muestraPlatillos(){
+	location.href = "admin.html"
+}
