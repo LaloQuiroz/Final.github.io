abe29587f5418055957e5281238a2a72fbd14ab6
LaloQuiroz <94099093+LaloQuiroz@users.noreply.github.com>
2021-12-10 23:16:28 -0600

    Add files via upload

---
+++ js/platillos.js
@@ -0,0 +1,45 @@
+import {
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  subeStorage
+} from "../lib/storage.js";
+import {
+  cod, getForánea, muestraError
+} from "../lib/util.js";
+import {
+  muestraUsuarios,
+  muestraPlatillos
+} from "./navegacion.js";
+
+const SIN_PLATILLOS = /* html */
+  `<option value="">
+    -- Sin Alumnos --
+  </option>`;
+
+const firestore = getFirestore();
+
+const daoRol = firestore.collection("Rol");
+const daoPlatillo = firestore.collection("Platillo");
+const daoUsuario = firestore.collection("Usuario");
+
+export async function
+  guardaFoto(evt, formData,
+    id) {
+  try {
+    evt.preventDefault();
+    const idPlatillo = getForánea(formData,"id");
+    const rolIds = formData.getAll("rolIds");
+    await daoUsuario.
+      doc(id).
+      set({
+        alumnoId,
+        rolIds
+      });
+    const avatar = formData.get("imagen");
+    await subeStorage(id, avatar);
+    muestraUsuarios();
+  } catch (e) {
+    muestraError(e);
+  }
+}
