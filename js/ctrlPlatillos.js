c3133e79c21c41322de75ff4f9f95ae8f95c4b17
LaloQuiroz <94099093+LaloQuiroz@users.noreply.github.com>
2021-12-10 21:21:35 -0600

    Add files via upload

---
+++ js/ctrlPlatillos.js
@@ -0,0 +1,52 @@
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  getString,
+  muestraError
+} from "../lib/util.js";
+import {
+  muestraPlatillos
+} from "./navegacion.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+
+const daoPlatillo = getFirestore().collection("Platillo");
+
+const forma = document["forma"];
+getAuth().onAuthStateChanged(protege, muestraError);
+
+async function protege(usuario) {
+  if (tieneRol(usuario,["Admnistrador"])) {
+    forma.addEventListener(
+      "submit", guarda);
+  }
+}
+
+async function guarda(evt) {
+  try {
+    evt.preventDefault();
+    const formData = new FormData(forma);
+
+    const id = getString(formData, "id").trim();  
+    const nombre = getString(formData, "nombre").trim();
+    const imagen = get(formData, "imagen");
+    const precio = getString(formData, "precio").trim();
+    const descrip = getString(formData, "descrip").trim();
+
+    const modelo = {
+      id,
+      nombre,
+      telefono,
+      precio,
+      descrip 
+    };
+    await daoPlatillo.
+      add(modelo);
+    muestraPlatillos();
+  } catch (e) {
+    muestraError(e);
+  }
+}
