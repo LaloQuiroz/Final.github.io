2b5560e131f8d349452613cf25828af658ed28e4
LaloQuiroz <94099093+LaloQuiroz@users.noreply.github.com>
2021-12-13 02:00:47 -0600

    Add files via upload

---
+++ js/ctrlCarrito.js
@@ -0,0 +1,26 @@
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  cod,
+  muestraError
+} from "../lib/util.js";
+
+getAuth().onAuthStateChanged(protege, muestraError);
+
+const firestore = getFirestore();
+const daoPlatillo = firestore.collection("Platillo");
+const forma = document["forma"];
+
+llena();
+
+ async function llena(){
+	try{
+		const doc = await daoPlatillo.get();
+		querySnapshot.forEach((doc) => { forma.pago.value = doc});
+	}catch(e){
+		console.log(e);
+	}
+}
+
